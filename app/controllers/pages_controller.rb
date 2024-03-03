dd330b704308f138ccd158179b84fd4cb069e850
Jared Champayne <j4champa@gmail.com>
2024-03-03 13:43:49 -0600

    basic device setup - ability to login and sign up

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  # This line is added to skip the authentication for the home page
+  # if you want to skip authentication for any other page, you can add it here ex: only: :home or except: :home
+    skip_before_action :authenticate_user!, only: :home
+
+  def home
+  end
+end
